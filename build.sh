2d184b35cf52f82992a6693451fcff7291927d0c
deepakkumar-gridindia <deepakkumar@Deepaks-MacBook-Pro.local>
2026-03-19 14:26:34 +0530

    Fix Render build setup

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
